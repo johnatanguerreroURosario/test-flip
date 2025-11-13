bc0919e750b9db72cd3d867c79f24a27507e1ebf
Johnatan Guerrero <johnatan.guerrero@urosario.edu.co>
2025-11-13 13:22:40 -0500

    fix-2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
+  build: {
+    assetsInlineLimit: 0,
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    }
+  }
 })
