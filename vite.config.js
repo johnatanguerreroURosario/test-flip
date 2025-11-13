82411e3d2a5f5e0b65e90587f7deaaa738c44a97
Johnatan Guerrero <johnatan.guerrero@urosario.edu.co>
2025-11-13 14:14:18 -0500

    test to relative path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://test-flip.vercel.app/assets/',
   plugins: [react()],
   publicDir: 'public',
   build: {
