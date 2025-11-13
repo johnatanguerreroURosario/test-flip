ed9f9952db6667844fa6c05e014bde4ab3d80540
Johnatan Guerrero <johnatan.guerrero@urosario.edu.co>
2025-11-13 14:32:51 -0500

    test cambio de base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://test-flip.vercel.app/assets/',
+  base: 'https://test-flip.vercel.app/',
   plugins: [react()],
   publicDir: 'public',
   build: {
